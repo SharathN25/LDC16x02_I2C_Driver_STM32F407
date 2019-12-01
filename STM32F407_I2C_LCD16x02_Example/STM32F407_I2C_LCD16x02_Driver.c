436653da11f0c21c64f6993ac4ced136f2d8ae05
Sharath N <sharathsharu25@gmail.com>
2019-12-01 21:32:07 +0530

    Updating example project

--- STM32F407_I2C_LCD16x02_Example/STM32F407_I2C_LCD16x02_Driver.c
+++ STM32F407_I2C_LCD16x02_Example/STM32F407_I2C_LCD16x02_Driver.c
@@ -3,18 +3,66 @@
   *@file   : STM32F407_I2C_LCD16x02_Driver.c
   *@author : Sharath N
   *@brief  : LCD driver source file for interfacing 16x02 LCD on STM32Fxx MCU using I2C Serial 
-	           Interface Module
+	         Interface Module
   ********************************************************************************************
 */	
 
-#include "stdint.h"
 #include "stm32f4xx_hal.h"
 #include "STM32F407_I2C_LCD16x02_Driver.h"
 
-/* !!! Make sure that I2C_HandleTypedef has same name in both in "main.c" and here !!! */
-extern I2C_HandleTypeDef myI2Chandle;
+/* I2C Handle */
+I2C_HandleTypeDef myI2Chandle;
 
 
+/********************* Private function for I2C initialization *************************/
+
+/* Configure GPIOs */
+static void GPIO_Config(void)
+{
+	//Enable Ports clocks
+	__HAL_RCC_GPIOD_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+	
+	//Init tyoeDef
+	GPIO_InitTypeDef myPinInit;
+
+	//I2C pins config
+	myPinInit.Pin = GPIO_PIN_6 |GPIO_PIN_7;
+	myPinInit.Mode = GPIO_MODE_AF_OD;
+	myPinInit.Pull = GPIO_PULLUP;
+	myPinInit.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+	myPinInit.Alternate = GPIO_AF4_I2C1;
+	HAL_GPIO_Init(GPIOB, &myPinInit);
+
+	//Systick interrupt enable for HAL_Delay function
+    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+    HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+	
+}
+
+
+/*Configure I2C Peripheral */
+static void I2C_Config(void)
+{
+	//Enable I2C peripheral clock
+	__HAL_RCC_I2C1_CLK_ENABLE();
+	
+	myI2Chandle.Instance = I2C1;
+	myI2Chandle.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+	myI2Chandle.Init.ClockSpeed = 100000;
+	myI2Chandle.Init.DualAddressMode = I2C_DUALADDRESS_DISABLED;
+	myI2Chandle.Init.DutyCycle = I2C_DUTYCYCLE_2;
+	myI2Chandle.Init.GeneralCallMode = I2C_GENERALCALL_DISABLED;
+	myI2Chandle.Init.NoStretchMode = I2C_NOSTRETCH_DISABLED;
+	myI2Chandle.Init.OwnAddress1 = 0;
+	myI2Chandle.Init.OwnAddress2 = 0;
+	HAL_I2C_Init(&myI2Chandle);
+}
+/**********************************************************************************/
+
+
+/********************************* LCD APIs ***************************************/
 /**
  * @brief Send Command to LCD
  * @param cmd : LCD commands 
@@ -98,12 +146,20 @@ void LCD_Send_Data(uint8_t data)
 
 
 
+
+
 /**
  * @brief  Initializes LCD
  * @retval None
  */
 void LCD_Init(void)
 {
+	/* Configure GPIOs */
+	GPIO_Config();
+
+	/*Configure I2C */
+	I2C_Config();
+
 	/* Wait for 15ms */
 	HAL_Delay(15);
 	
@@ -143,6 +199,51 @@ void LCD_Send_String(char *str)
 }
 
 
+/**
+ * @brief Clears screen first, then displays the given string
+ * @param str: pointer to strings
+ * @retval None
+ */
+void LCD_Clear_Then_Display(char *str)
+{
+	LCD_Send_Cmd(LCD_CLEAR_DISPLAY);
+	LCD_Send_String(str);
+}
 
 
+
+/**
+ * @brief Display the strings on Line1
+ * @param str: pointer to strings
+ * @retval None
+ */
+void LCD_Send_String_On_Line1(char *str)
+{
+	LCD_Send_Cmd(LCD_SET_ROW1_COL1);
+	LCD_Send_String(str);
+}
+
+
+/**
+ * @brief Display the strings on Line2
+ * @param str: pointer to strings
+ * @retval None
+ */
+void LCD_Send_String_On_Line2(char *str)
+{
+	LCD_Send_Cmd(LCD_SET_ROW2_COL1);
+	LCD_Send_String(str);
+}
+
+
+/********************************* LCD APIs - END ************************************/
+
+
+
+/*Systick Handler*/
+void SysTick_Handler(void)
+{
+  HAL_IncTick();
+  HAL_SYSTICK_IRQHandler();
+}
 
